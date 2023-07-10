da8171527c57b38c886e4cdcf4364539f5cef354
Sol8888 <131205430+Sol8888@users.noreply.github.com>
2023-07-10 15:10:15 -0500

    calculo del promedio, comparación estudiantes

--- ExamenP3.c
+++ ExamenP3.c
@@ -2,13 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LENGTH 50
+#define MAX_LARGO 50
 
 typedef struct {
     int codigo;
-    char nombre[MAX_LENGTH];
-    char carrera[MAX_LENGTH];
-    float nota1;
-    float nota2;
-    float nota3;
+    char nombre[MAX_LARGO];
+    char carrera[MAX_LARGO];
+    float N1;
+    float N2;
+    float N3;
 } Estudiante;
+
+float calcularPromedio(float N1, float N2, float N3,) {
+     return (N1 + N2+ N3)/3;
+}
+
+int compararEstu( const void* estu1, const void* estu2) {
+    Estudiante* e1 = (Estudiante*)estu1;
+    Estudiante* e2 = (Estudiante*)estu2;
+    return e2->codigo - e1->codigo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
