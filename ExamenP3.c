77b3216a68aa09724042e2dd116926702f8357b7
Sol8888 <131205430+Sol8888@users.noreply.github.com>
2023-07-10 16:05:36 -0500

    cambio de nombre de la estructura completado
    se cambio de nombre la estructura de estudiante a estu, para que no se confunda con estudiantes (variable)

--- ExamenP3.c
+++ ExamenP3.c
@@ -11,15 +11,15 @@ typedef struct {
     float N1;
     float N2;
     float N3;
-} Estudiante;
+} Estu;
 
 float calculoProm(float N1, float N2, float N3) {
     return (N1 + N2+ N3)/3;
 }
 
 int compararEstu( const void* estu1, const void* estu2) {
-    Estudiante* e1 = (Estudiante*)estu1;
-    Estudiante* e2 = (Estudiante*)estu2;
+    Estu* e1 = (Estu*)estu1;
+    Estu* e2 = (Estu*)estu2;
     return e2->numA - e1->numA;
 }
 
@@ -27,7 +27,7 @@ int compararEstu( const void* estu1, const void* estu2) {
 int main() {
 
     FILE *archiE, *archiS;
-    Estudiante *estudiantes;
+    Estu *estudiantes;
     int numEstu;
     archiE = fopen("alumnos.txt", "r");
     if (archiE == NULL) {
@@ -37,7 +37,7 @@ int main() {
 
     fscanf(archiE, "%d", &numEstu);
 
-    estudiantes = (Estudiante*)malloc(numEstu * sizeof(Estudiante));
+    estudiantes = (Estu*)malloc(numEstu * sizeof(Estu));
     if (estudiantes == NULL) {
         printf("No se pudo asignar memoria.\n");
         return 1;
@@ -51,7 +51,7 @@ int main() {
 
     fclose(archiE);
 
-    qsort(estudiantes, numEstu, sizeof(Estudiante), compararEstu);
+    qsort(estudiantes, numEstu, sizeof(Estu), compararEstu);
 
     archiS = fopen ("estudiantes_en_orden.txt", "w");
     
@@ -69,18 +69,18 @@ int main() {
         sumaProm += promedio;
         fprintf (archiS, "%d, %s, %s, %.2f; %.2f; %2f\n",
             estudiantes[i]. numA, estudiantes[i]. nombre, estudiantes[i].carrera, 
-            estudiantes[i].N1, estudiantes[i].N2, estudiantes[i].N3, promedio,);
+            estudiantes[i].N1, estudiantes[i].N2, estudiantes[i].N3, promedio);
     }
 
     float promTotal = sumaProm / numEstu;
-    fprintf (archiS "Promedio total: %.2f\n", promTotal);
+    fprintf (archiS, "Promedio total: %.2f\n", promTotal);
 
-    fclose(archiS)
+    fclose(archiS);
 
     
     free(estudiantes);
 
-    printf("El proceso termino.\n);
+    printf("El proceso termino.\n");
 
     return 0;
 
